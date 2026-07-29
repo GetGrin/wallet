b3e90596a8e7197d5d92185cf525bacd238c83d3
ardocrat <ardocrat@gri.mw>
2026-07-29 18:46:17 +0300

    fix: set local chain type for tests

--- controller/tests/common/mod.rs
+++ controller/tests/common/mod.rs
@@ -84,6 +84,7 @@ pub fn clean_output_dir(test_dir: &str) {
 pub fn setup(test_dir: &str) {
 	util::init_test_logger();
 	clean_output_dir(test_dir);
+	global::set_local_chain_type(ChainTypes::AutomatedTesting);
 	setup_global_chain_type();
 }
 
